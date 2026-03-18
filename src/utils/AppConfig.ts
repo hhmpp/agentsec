49c2c5add8f3c88b9ae73ec4c95944e9f0fe3348
minpeng hui <minpenghui@minpengdeMacBook-Air.local>
2026-03-18 18:10:49 +0900

    feat: init AgentSec

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -4,15 +4,21 @@ import { BILLING_INTERVAL, type PricingPlan } from '@/types/Subscription';
 
 const localePrefix: LocalePrefix = 'as-needed';
 
-// FIXME: Update this configuration file based on your project information
 export const AppConfig = {
-  name: 'SaaS Template',
+  name: 'AgentSec',
   locales: [
     {
       id: 'en',
       name: 'English',
     },
-    { id: 'fr', name: 'Français' },
+    {
+      id: 'zh',
+      name: '中文',
+    },
+    {
+      id: 'ja',
+      name: '日本語',
+    },
   ],
   defaultLocale: 'en',
   localePrefix,
@@ -35,40 +41,38 @@ export const PricingPlanList: Record<string, PricingPlan> = {
     devPriceId: '',
     prodPriceId: '',
     features: {
-      teamMember: 2,
-      website: 2,
-      storage: 2,
-      transfer: 2,
+      teamMember: 1,
+      website: 3,
+      storage: 3,
+      transfer: 3,
     },
   },
   [PLAN_ID.PREMIUM]: {
     id: PLAN_ID.PREMIUM,
-    price: 79,
+    price: 49,
     interval: BILLING_INTERVAL.MONTH,
-    testPriceId: 'price_premium_test', // Use for testing
-    // FIXME: Update the price ID, you can create it after running `npm run stripe:setup-price`
+    testPriceId: 'price_premium_test',
     devPriceId: 'price_1PNksvKOp3DEwzQlGOXO7YBK',
     prodPriceId: '',
     features: {
-      teamMember: 5,
-      website: 5,
-      storage: 5,
-      transfer: 5,
+      teamMember: 10,
+      website: 20,
+      storage: 20,
+      transfer: 20,
     },
   },
   [PLAN_ID.ENTERPRISE]: {
     id: PLAN_ID.ENTERPRISE,
-    price: 199,
+    price: 299,
     interval: BILLING_INTERVAL.MONTH,
-    testPriceId: 'price_enterprise_test', // Use for testing
-    // FIXME: Update the price ID, you can create it after running `npm run stripe:setup-price`
+    testPriceId: 'price_enterprise_test',
     devPriceId: 'price_1PNksvKOp3DEwzQli9IvXzgb',
     prodPriceId: 'price_123',
     features: {
-      teamMember: 100,
-      website: 100,
-      storage: 100,
-      transfer: 100,
+      teamMember: 999,
+      website: 999,
+      storage: 999,
+      transfer: 999,
     },
   },
 };
